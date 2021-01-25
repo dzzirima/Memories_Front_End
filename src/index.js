59eee683a0f1d9cba1c4309abbc55f53d32e16e6
David Zirima <davidtzirima@gmail.com>
2021-01-25 12:33:56 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// this is the file we are goin to connet the react app to the html file
+import react from 'react'
+import reactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './App'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(<App />, document.getElementById('root'));
