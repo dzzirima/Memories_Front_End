7b101e20173b39d99f813b70bba241e55b720d34
David Zirima <davidtzirima@gmail.com>
2021-01-25 22:47:51 +0200

    stabileze

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import react from 'react'
 
+
 const App = () =>{
 
     return(
         <div>
             <h1> Front end working well..</h1>
+            
+            <p>This is  html component ....</p>
         </div>
     )
 }
-
 
 export  default App
