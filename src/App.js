c13461a7120004cc1526371a08aed9c43da446c8
David Zirima <davidtzirima@gmail.com>
2021-01-27 00:06:43 +0200

    working redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import react from 'react'
+import React,{useEffect} from 'react'
 import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+import {useDispatch} from 'react-redux'
 
-
+import {getPosts} from './actions/posts'
 import memories from './images/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'
 
 
-
 const App = () =>{
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() =>{
+        dispatch(getPosts());
+    },[dispatch])
+
 
     return(
        <Container maxidth = "lg"  >
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // this is the file we are goin to connet the react app to the html file
-import react from 'react'
+
 import reactDom from 'react-dom'
+
+import {Provider} from 'react-redux'
+import{createStore,applyMiddleware,compose} from 'redux'
+import  thunk from 'redux-thunk'
+import reducers from './reducers'
 
 import App from './App'
 
-reactDom.render(<App />, document.getElementById('root'));
+const store = createStore(reducers,compose(applyMiddleware(thunk)))
+
+reactDom.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+ document.getElementById('root'));
