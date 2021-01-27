d1f8f0156aad7b9275b4599b76fcc854db90f592
David Zirima <davidtzirima@gmail.com>
2021-01-28 01:58:38 +0200

    working code ready for deployment ...

---
+++ src/Form.js
@@ -0,0 +1,18 @@
+import react from 'react'
+
+const Form = () =>{
+
+    return(
+        <div>
+            <form>
+                <button>btn</button>
+                <button>btn</button>
+                <button>btn</button>
+
+            </form>
+        
+        </div>
+    )
+}
+
+export  default Form
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,18 +1,19 @@
+import {FETCH_ALL,CREATE,UPDATE,DELETE} from '../constants/actionTypes'
+
 export default  (posts = [],action) =>{
 
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
 
             // by putting them one on top of the other they perform the same thing
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload._id : post)
 
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload
         
-        case 'CREATE':
+        case CREATE:
             return [...posts,action.payload]
             
         default:
