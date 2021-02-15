a2d7018dbb1abe5cb1ed4c8097efc2df2d2bfe20
David Zirima <davidtzirima@gmail.com>
2021-02-15 23:29:03 +0200

    displaying the user details using the object return form google

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {AppBar,Avatar,Button,Toolbar,Typography} from '@material-ui/core'
 import {Link} from 'react-router-dom'
 import useStyles from './Styles'
@@ -7,8 +7,11 @@ import memories from '../../images/memories.png'
 
 const Navbar = () => {
     const classes = useStyles()
+    //gettig the user profile
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+    console.log(user)
 
-    const user = null;
+
 
     return (
         <AppBar className = {classes.appBar} position ="static" color = "inherit">
